c05505e7c7d1388298a086ce1c80ce459d7ffd98
zhushuai <cinnabar_s@sina.com>
2019-06-27 11:03:16 +0800

    #标大大  增加数据组企业工商接口

---
+++ src/main/java/com/silita/biaodaa/model/TbCompanyUpdate.java
@@ -0,0 +1,45 @@
+package com.silita.biaodaa.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Setter
+@Getter
+public class TbCompanyUpdate {
+    /**
+     * 主键id
+     */
+    private Integer pkid;
+
+    /**
+     * 企业id
+     */
+    private String comId;
+
+    /**
+     * 企业名称
+     */
+    private String comName;
+
+    /**
+     * 更新人用户id
+     */
+    private String userId;
+
+    /**
+     * 更新状态(0:正在更新中，1：初始化，2：更新完成)
+     */
+    private Integer isUpdated;
+
+    /**
+     * 创建时间
+     */
+    private Date created;
+
+    /**
+     * 修改时间
+     */
+    private Date updated;
+}
