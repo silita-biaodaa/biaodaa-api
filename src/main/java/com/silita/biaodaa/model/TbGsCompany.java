55911d06fec82e2139cbf72520285f7640849a5a
zhushuai <cinnabar_s@sina.com>
2019-06-27 16:22:03 +0800

    #标大大  增加一键更新接口

---
+++ src/main/java/com/silita/biaodaa/model/TbGsCompany.java
@@ -0,0 +1,55 @@
+package com.silita.biaodaa.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Setter
+@Getter
+public class TbGsCompany {
+    /**
+     * 主键id
+     */
+    private Integer pkid;
+
+    /**
+     * 企业id
+     */
+    private String comId;
+
+    /**
+     * 企业名称
+     */
+    private String comName;
+
+    /**
+     * 更新时间
+     */
+    private Date updated;
+
+    /**
+     * 基本信息
+     */
+    private String basic;
+
+    /**
+     * 股东信息
+     */
+    private String partner;
+
+    /**
+     * 主要人员
+     */
+    private String personnel;
+
+    /**
+     * 变更记录
+     */
+    private String changRecord;
+
+    /**
+     * 行政处罚
+     */
+    private String punish;
+}
